f20721e9b524ff1b45ea999db263dbc52e015938
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2023-01-08 20:12:13 +0100

    finish

---
+++ app.js
@@ -0,0 +1,21 @@
+const contador = document.getElementById("counter");
+const sumar = document.getElementById("incr");
+const classes = ["f0", "f1", "f2", "f3", "f4", "f5"];
+let classIndex = 0;
+let previousClass = classIndex;
+
+sumar.addEventListener("click", function () {
+  classIndex++;
+  classIndex = classIndex == classes.length ? classes.length - 1 : classIndex;
+  changeClass(previousClass, classIndex);
+  contador.innerHTML = classIndex;
+  if (classIndex == 5) {
+    imagen = `<img src='https://cdn.dribbble.com/users/234969/screenshots/5404808/medallion_burst_animation.gif' alt='gift'/>`;
+    document.write(imagen);
+  }
+});
+
+function changeClass(next) {
+  let htmlElement = document.getElementById("balloon");
+  htmlElement.classList.add(classes[next]);
+}
