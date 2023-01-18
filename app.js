1805d3fe3910ba84c4b18944522da4de084b9ed9
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2023-01-18 20:40:52 +0100

    regalito a Sofy y a Valen

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const contador = document.getElementById("counter");
 const sumar = document.getElementById("incr");
-const classes = ["f0", "f1", "f2", "f3", "f4", "f5"];
+const classes = [
+  "f0",
+  "f1",
+  "f2",
+  "f3",
+  "f4",
+  "f5",
+  "f6",
+  "f7",
+  "f8",
+  "f9",
+  "f10",
+];
 let classIndex = 0;
 let previousClass = classIndex;
+let años;
+function saludar(params) {
+  años = prompt("Cuantos años cumples?");
+  alert(`¡Aprieta el botón ${años} veces!`);
+}
+saludar();
 
 sumar.addEventListener("click", function () {
   classIndex++;
   classIndex = classIndex == classes.length ? classes.length - 1 : classIndex;
   changeClass(previousClass, classIndex);
   contador.innerHTML = classIndex;
-  if (classIndex == 5) {
-    imagen = `<img class='img' src='https://cdn.dribbble.com/users/234969/screenshots/5404808/medallion_burst_animation.gif' alt='gift'/>`;
+  if (classIndex == años) {
+    imagen = `<img id='minie' width="80%" src='./tenor.gif' alt='git minie'/>`;
     document.write(imagen);
   }
 });
